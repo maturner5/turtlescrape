cb08c99953a37ebd9d6170456aa9b9a762706c5e
Matt Turner <demexus2@gmail.com>
2021-01-23 13:49:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="turtlescrape",
-    version="1.0.0",
+    version="1.0.1",
     author="Matt Turner",
     author_email="demexus2@gmail.com",
     description="A simple library used to make web scraping take up less lines"
