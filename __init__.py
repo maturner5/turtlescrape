92009f80c746218b5da9fa0686d42e8f81c588d2
Matt Turner <demexus2@gmail.com>
2021-01-23 14:55:03 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,13 +9,12 @@ import sys
 # makes error message only one line
 sys.tracebacklimit = 0 
 
-url = 'https://www.marketwatch.com/investing/stock/tsla?mod=quote_search'
 # function to search for text using html tags as arguments
 def turtlescrape(url, *args, **kwargs):
     try:
         page = urlopen(url)
     except:
-        print("Failed to load page. This is probably because the site you tried"                " to load doesn't allow you to scrape it.")
+        print("Failed to load page. This is probably because the site you tried to load doesn't allow you to scrape it.")
     text = page.read()
     soup = BeautifulSoup(text, 'html.parser')
     if (soup):
